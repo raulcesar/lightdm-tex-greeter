82599eabe2b7eb91de94e469e713dcba1591ab6c
Raul Teixeira <raul.teixeira@camara.leg.br>
2014-08-18 11:51:40 -0300

    Housekeeping

--- themes/webkitsimple/script.js
+++ themes/webkitsimple/script.js
@@ -6,119 +6,119 @@ var selected_user = null;
 
 // called when the greeter asks to show a login prompt for a user
 function show_prompt(text) {
-  var password_container = document.querySelector("#password_container");
-  var password_entry = document.querySelector("#password_entry");
+   var password_container = document.querySelector("#password_container");
+   var password_entry = document.querySelector("#password_entry");
 
-  if (!isVisible(password_container)) {
-    var users = document.querySelectorAll(".user");
-    var user_node = document.querySelector("#"+selected_user);
-    var rect = user_node.getClientRects()[0];
-    var parentRect = user_node.parentElement.getClientRects()[0];
-    var center = parentRect.width/2;
-    var left = center - rect.width/2 - rect.left;
-    var i;
-    if (left < 5 && left > -5) {
-      left = 0;
-    }
-    for (i = 0; i < users.length; i++) {
-      var node = users[i];
-      setVisible(node, node.id === selected_user);
-      node.style.left= left;
-    }
+   if (!isVisible(password_container)) {
+      var users = document.querySelectorAll(".user");
+      var user_node = document.querySelector("#" + selected_user);
+      var rect = user_node.getClientRects()[0];
+      var parentRect = user_node.parentElement.getClientRects()[0];
+      var center = parentRect.width / 2;
+      var left = center - rect.width / 2 - rect.left;
+      var i;
+      if (left < 5 && left > -5) {
+         left = 0;
+      }
+      for (i = 0; i < users.length; i++) {
+         var node = users[i];
+         setVisible(node, node.id === selected_user);
+         node.style.left = left;
+      }
 
-    setVisible(password_container, true);
-    password_entry.placeholder= text.replace(":", "");
-  }
-  password_entry.value= "";
-  password_entry.focus();
+      setVisible(password_container, true);
+      password_entry.placeholder = text.replace(":", "");
+   }
+   password_entry.value = "";
+   password_entry.focus();
 }
 
 // called when the greeter asks to show a message
 function show_message(text) {
-  var message = document.querySelector("#message_content");
-  message.innerHTML= text;
-  if (text) {
-    document.querySelector("#message").classList.remove("hidden");
-  } else {
-    document.querySelector("#message").classList.add("hidden");
-  }
-  message.classList.remove("error");
+   var message = document.querySelector("#message_content");
+   message.innerHTML = text;
+   if (text) {
+      document.querySelector("#message").classList.remove("hidden");
+   } else {
+      document.querySelector("#message").classList.add("hidden");
+   }
+   message.classList.remove("error");
 }
 
 // called when the greeter asks to show an error
 function show_error(text) {
-  show_message(text);
-  var message = document.querySelector("#message_content");
-  message.classList.add("error");
+   show_message(text);
+   var message = document.querySelector("#message_content");
+   message.classList.add("error");
 }
 
 // called when the greeter is finished the authentication request
 function authentication_complete() {
-  if (lightdm.is_authenticated) {
-    lightdm.login(lightdm.authentication_user, lightdm.default_session);
-  } else {
-    show_error("Authentication Failed");
-    start_authentication(selected_user);
-  }
+   if (lightdm.is_authenticated) {
+      lightdm.login(lightdm.authentication_user, lightdm.default_session);
+   } else {
+      show_error("Authentication Failed");
+      start_authentication(selected_user);
+   }
 }
 
 // called when the greeter wants us to perform a timed login
 function timed_login() {
-  lightdm.login (lightdm.timed_login_user);
-  //setTimeout('throbber()', 1000);
+   lightdm.login(lightdm.timed_login_user);
+   //setTimeout('throbber()', 1000);
 }
 
 //////////////////////////////
 // Implementation
 //////////////////////////////
 function start_authentication(username) {
-  lightdm.cancel_timed_login();
-  selected_user = username;
-  lightdm.start_authentication(username);
+   lightdm.cancel_timed_login();
+   selected_user = username;
+   lightdm.start_authentication(username);
 }
 
-   //noinspection JSUnusedGlobalSymbols
+//noinspection JSUnusedGlobalSymbols
 function provide_secret() {
-     show_message("Logging in...");
-     entry = document.querySelector('#password_entry');
-     lightdm.provide_secret(entry.value);
-   }
+   show_message("Logging in...");
+   entry = document.querySelector('#password_entry');
+   lightdm.provide_secret(entry.value);
+}
 
 function show_users() {
-  var users = document.querySelectorAll(".user");
-  var i;
-  for (i= 0; i < users.length; i++) {
-    setVisible(users[i], true);
-    users[i].style.left = 0;
-  }
-  setVisible(document.querySelector("#password_container"), false);
-  selected_user = null;
+   var users = document.querySelectorAll(".user");
+   var i;
+   for (i = 0; i < users.length; i++) {
+      setVisible(users[i], true);
+      users[i].style.left = 0;
+   }
+   setVisible(document.querySelector("#password_container"), false);
+   selected_user = null;
 }
 
 function user_clicked(event) {
-  if (selected_user !== null) {
-    selected_user = null;
-    lightdm.cancel_authentication();
-    show_users();
-  } else {
-    selected_user = event.currentTarget.id;
-    start_authentication(event.currentTarget.id);
-  }
-  show_message("");
-  event.stopPropagation();
-  return false;
+   if (selected_user !== null) {
+      selected_user = null;
+      lightdm.cancel_authentication();
+      show_users();
+   } else {
+      selected_user = event.currentTarget.id;
+      start_authentication(event.currentTarget.id);
+   }
+   show_message("");
+   event.stopPropagation();
+   return false;
 }
 
 function setVisible(element, visible) {
-  if (visible) {
-    element.classList.remove("hidden");
-  } else {
-    element.classList.add("hidden");
-  }
+   if (visible) {
+      element.classList.remove("hidden");
+   } else {
+      element.classList.add("hidden");
+   }
 }
 
 function isVisible(element) {
-  return !element.classList.contains("hidden");
+   return !element.classList.contains("hidden");
 }
 
 //////////////////////////////////
@@ -126,44 +126,44 @@ function isVisible(element) {
 //////////////////////////////////
 
 function initialize() {
-  show_message("");
-  initialize_users();
+   show_message("");
+   initialize_users();
 }
 
 function on_image_error(e) {
-  e.currentTarget.src = "monkeyavatar.svg";
+   e.currentTarget.src = "monkeyavatar.svg";
 }
 
 function initialize_users() {
-  var template = document.querySelector("#user_template");
-  var parent = template.parentElement;
-  parent.removeChild(template);
+   var template = document.querySelector("#user_template");
+   var parent = template.parentElement;
+   parent.removeChild(template);
 
-  for (i = 0; i < lightdm.users.length; i += 1) {
-    user = lightdm.users[i];
-    userNode = template.cloneNode(true);
-
-
-     var fooinfo = lightdm.getUserProperty(user.name, 'fooinfo');
-     fooinfo = (fooinfo !== null) ? ' (' + fooinfo + ')' : '';
+   for (i = 0; i < lightdm.users.length; i += 1) {
+      user = lightdm.users[i];
+      userNode = template.cloneNode(true);
 
 
-     var image = userNode.querySelectorAll(".user_image")[0];
-    var name = userNode.querySelectorAll(".user_name")[0];
-    name.innerHTML = user.display_name + fooinfo;
+      var fooinfo = lightdm.getUserProperty(user.name, 'fooinfo');
+      fooinfo = (fooinfo !== null) ? ' (' + fooinfo + ')' : '';
 
-    if (user.image) {
-      image.src = user.image;
-      image.onerror = on_image_error;
-    } else {
-      image.src = "monkeyavatar.svg";
-    }
 
-    userNode.id = user.name;
-    userNode.onclick = user_clicked;
-    parent.appendChild(userNode);
-  }
-  setTimeout(show_users, 400);
+      var image = userNode.querySelectorAll(".user_image")[0];
+      var name = userNode.querySelectorAll(".user_name")[0];
+      name.innerHTML = user.display_name + fooinfo;
+
+      if (user.image) {
+         image.src = user.image;
+         image.onerror = on_image_error;
+      } else {
+         image.src = "monkeyavatar.svg";
+      }
+
+      userNode.id = user.name;
+      userNode.onclick = user_clicked;
+      parent.appendChild(userNode);
+   }
+   setTimeout(show_users, 400);
 }
 
 //function add_action(id, name, image, clickhandler, template, parent) {
